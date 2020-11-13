969a5b5a5e52ee80909d05b5def12d956a8d810b
Dave Matney <matneyx@gmail.com>
2020-11-12 19:56:17 -0700

    More styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Navbar from './common/Navbar';
 import GolarionActionEconomy from './rules/combat/GolarionActionEconomy';
+import styled from 'styled-components';
+
+const MainBody = styled.div`
+  background-color: #EEE5CE;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 50px;
+  overflow: scroll;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <GolarionActionEconomy />
+        <MainBody>
+          <GolarionActionEconomy />
+        </MainBody>
       </div>
     );
   }
--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -5,7 +5,17 @@ import { Navbar } from 'react-bootstrap';
 import logo from './../images/MimicsAndXorn-Simple.png';
 
 const StyledNavbar = styled(Navbar)`
-  height: 90px;
+  height: 50px;
+  box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  width: 100%
+`;
+
+const StyledImg = styled.img`
+  max-height: 50%;
+  max-width: 50%;
 `;
 
 const menus = {
@@ -26,7 +36,7 @@ const menus = {
 export default () => (
   <StyledNavbar bg="light" expand="lg">
     <Navbar.Brand href="#home">
-      <img
+      <StyledImg
         src={logo}
         className="d-inline-block align-top"
         alt="Mimics & Xorn" />
